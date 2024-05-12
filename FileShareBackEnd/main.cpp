3a886e82341dc2577cd91017cff55276bd069e17
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-05-12 22:17:59 +0300

    Added libcpr to be able to send http requests and updated visual studio project configuration.

--- FileShareBackEnd/main.cpp
+++ FileShareBackEnd/main.cpp
@@ -1,13 +1,21 @@
 #include <cstdio>
 #include <crow_all.h>
-
+#include <cpr/cpr.h>
 
 
 int main()
 {
+    cpr::Response responseToGet = cpr::Get(cpr::Url("localhost"),
+        cpr::Authentication("user", "pass", cpr::AuthMode::BASIC));
+
+    std::cout << responseToGet.status_code;
+
     crow::SimpleApp app;
     CROW_ROUTE(app, "/")([]() {
         return "Hello world";
         });
     app.port(65000).multithreaded().run();
+    
+    
+    
 }
